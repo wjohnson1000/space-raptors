d6daa42d8f5e5dd1aeba643d6f58544f13a5af6b
William Johnson <wjohnson1000@gmail.com>
2015-12-20 18:26:08 -0700

    trying to write swim function

---
+++ Assets/Swimm.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var swim :float;
+var rigidBody :Rigidbody2D;
+
+function Start () {
+
+}
+
+
+
+
+function Swim() {
+	if(rigidBody.tag=="Player" && Input.GetAxis("Vertical") > 0){
+			rigidBody.AddForce(Vector2(0, swim));
+		}
+	
+
+
+}
